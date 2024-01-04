5efe9810aaf3605c59a6c99ec386380ed5f7af5a
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-01-04 21:55:39 +0600

    contact list added

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 export default function ContactList() {
@@ -8,7 +8,7 @@ export default function ContactList() {
       name: 'Humayra Akter',
       status: 'Making your gPay smooth',
       imageUrl:
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fwoman-avatar&psig=AOvVaw22fTtbpfzof4BwRZlNIKGj&ust=1704387945939000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCJDFzIXawYMDFQAAAAAdAAAAABAE',
+        'hhttps://media.istockphoto.com/id/1363398400/photo/woman-traveler-in-europa-alhambra-in-spain.jpg?s=612x612&w=0&k=20&c=Z14eQ_IJywp7COkEQvAhxhiOagmqupQaO_RuRg5kEzM=',
     },
     {
       uid: 2,
@@ -34,9 +34,55 @@ export default function ContactList() {
   ];
   return (
     <View>
-      <Text>ContactList</Text>
+      <Text style={styles.headingText}>Contact List</Text>
+      <ScrollView style={styles.container} scrollEnabled={false}>
+        {contacts.map(({uid, name, status, imageUrl}) => (
+          <View key={uid} style={styles.userCard}>
+            <Image
+              source={{
+                uri: imageUrl,
+              }}
+              style={styles.userImage}
+            />
+            <View>
+              <Text style={styles.userName}>{name} </Text>
+              <Text style={styles.userStatus}>{status} </Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 12,
+  },
+  container: {
+    paddingHorizontal: 16,
+    marginBottom: 4,
+  },
+  userCard: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 3,
+  },
+  userImage: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    marginRight: 14,
+  },
+  userName: {
+    fontSize: 16,
+    // fontWeight: 'bold',
+    color: '#000',
+  },
+  userStatus: {
+    fontSize: 12,
+  },
+});
